13d1e50a03b619f3bf78a66df60ea185daea3d09
Shylaja2021 <98077969+Shylaja2021@users.noreply.github.com>
2022-02-07 12:22:51 +0530

    display variables and string

---
+++ varstrbasics.py
@@ -0,0 +1,21 @@
+
+#variables
+count=10
+mile=10000.000
+str="hello world"
+
+print("count : ",count,end=" ")
+print("mile : ",mile,end=" ")
+print("str : ",str)
+
+# string
+
+str="Python Programming"
+
+print("str[0] : ",str[0])
+print("str[2:5] : ",str[2:5])
+print("str[2:] : ",str[2:])
+print("str*2 : ",str*2)
+print("str+test : ",str+"test")
+print("str[-1] : ",str[-1])
+print("str[-18:] : ",str[-18:])
